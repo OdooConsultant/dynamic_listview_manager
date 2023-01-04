a6e22a3658bb77fc5e83f9940a0c9b3c8ea65b8b
mohamed.rahmouni <mohamed.rahmouni>
2023-01-04 14:55:51 +0100

    First commit

---
+++ dynamic_listview_manager/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Dynamic ListView Advance',
+    'summary': 'SU Dynamic ListView Advance',
+    'version': '10.0.0.0',
+    'category': 'Web',
+    'description': """
+        Dynamic ListView Advance
+    """,
+    'author': "Yee Software",
+    'depends': ['web'],
+    'data': [
+        'views/templates.xml',
+        'security/show_fields_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'qweb': [
+        'static/src/xml/base.xml',
+    ],
+    'license': 'OPL-1',
+    'images': ['images/main_screen.jpg'],
+    'price': 110,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'images': [
+        'static/description/module_image.png',
+    ],
+}
---
+++ dynamic_listview_manager/static/src/js/list_view.js
@@ -0,0 +1,81 @@
+odoo.define('dynamic_listview_advance_odoo_v81.dynamic_listview', function(require) {
+    var Model = require('web.Model');
+    var session = require('web.session');
+    var ListView = require('web.ListView');
+
+    ListView.include({
+        render_buttons: function($node) {
+            this._super($node);
+            this.uid = session.uid;
+            if (this.$buttons) {
+                this.$buttons.on('click', '.su_fields_show li', this.proxy('onClickShowField'));
+                this.$buttons.on('click', '.update_fields_show', this.proxy('updateShowField'));
+                this.$buttons.on('keypress', '.su_dropdown li > input', this.proxy('onChangeStringField'));
+                this.$buttons.on('focusout', '.su_dropdown li > input', this.proxy('onFocusOutTextField'));
+                this.$buttons.on('click', '.su_fields_show li > span', this.proxy('onClickSpanCheck'));
+                this.$buttons.on('click', '#apply_for_all_user', this.proxy('onClickApplyAll'));
+                this.$buttons.find('#ul_fields_show').sortable();
+                this.$buttons.find('#ul_fields_show').disableSelection();
+            }
+        },
+        _get_node_string: function(field_name) {
+            var field = this.viewInfo.fields[field_name];
+            var _field = this.viewInfo.fieldsInfo.list[field_name];
+            var result = field.string;
+            if (_field.hasOwnProperty("string")) {
+                result = _field.string;
+            }
+            return result;
+        },
+        onClickApplyAll: function(e){
+            e.stopPropagation();
+        },
+        onClickSpanCheck: function (e) {
+            var self = $(e.currentTarget);
+            if (e.currentTarget.className.search('span_ticked') >= 0){
+                self.parent().removeClass("selected");
+                self.removeClass("span_ticked");
+            }
+            e.stopPropagation();
+        },
+        onFocusOutTextField: function (e) {
+            var self = $(e.currentTarget);
+            self.removeClass("display-block");
+            self.parent().find('a').removeClass("display-none");
+        },
+        onChangeStringField: function (e) {
+            var self = $(e.currentTarget);
+            var text = self.val() + e.key;
+            self.parent().find('a').text(text);
+        },
+        getFieldsShow: function() {
+            var fields_show = [];
+            var sequence = 1;
+            _(this.$buttons.find(".su_fields_show li.selected")).each(function(result) {
+                var $result = $(result);
+                fields_show.push({string: $result.find('input').val().trim(), sequence: sequence, name: $result.attr("name")});
+                sequence += 1;
+            });
+            return fields_show;
+        },
+        updateShowField: function () {
+            // var self = this;
+            var values = {model: this.model, view_id: this.fields_view.view_id, fields_show: this.getFieldsShow(),
+                for_all_user: this.$buttons.find("#apply_for_all_user:checked").length ? true : false};
+            new Model("show.field").call('change_fields',  [values]).done(function (result) {
+                location.reload();
+            });
+        },
+        onClickShowField: function(e){
+            e.stopPropagation();
+            var self = $(e.currentTarget);
+            if (e.currentTarget.className.search('selected') < 0){
+                self.addClass("selected");
+                self.find('span').addClass("span_ticked");
+            }else{
+                self.find('input').addClass("display-block");
+                self.find('a').addClass("display-none");
+            }
+        }
+    });
+});
