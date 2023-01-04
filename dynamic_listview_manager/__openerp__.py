2facfe48e278465b09968b09ea08d7c37003813d
mohamed.rahmouni <mohamed.rahmouni>
2023-01-04 14:50:05 +0100

    First commit

---
+++ dynamic_listview_manager/__openerp__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Dynamic ListView Advance',
+    'summary': 'Dynamic ListView Advance',
+    'version': '8.0.0.0',
+    'category': 'Web',
+    'description': """
+        Dynamic ListView Advance. Change The Odoo List view On the fly without any technical knowledge
+    """,
+    'author': "Yee Software",
+    'depends': ['web'],
+    'data': ['views/templates.xml',
+             'security/show_fields_security.xml',
+             'security/ir.model.access.csv'],
+    'price': 110,
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'qweb': ['static/src/xml/base.xml'],
+    'images': [
+        'static/description/module_image.png',
+    ],
+}
